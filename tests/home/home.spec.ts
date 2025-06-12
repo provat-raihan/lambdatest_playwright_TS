bb0eeb6e4257431ee18e0422af854527b33a276f
Meraj Hossain Mukto <merajhossainmukto@Merajs-MacBook-Air.local>
2025-06-13 02:56:30 +0600

    update all the functions and pom

--- tests/home/home.spec.ts
+++ tests/home/home.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "../../utilities/fixtures";
 import { ExpectedValueProvider } from "../../utilities/valueProvider";
+import homeData from "../../testData/home.json";
 
 class HomePageTest extends ExpectedValueProvider {
   constructor() {
@@ -8,18 +9,16 @@ class HomePageTest extends ExpectedValueProvider {
 
   runTest() {
     test.describe("Homepage functionality test", () => {
-      test.beforeEach(async ({ runner, envData }) => {
+      test.beforeEach(async ({ runner, envData, homePage }) => {
         await runner.navigateTo(envData.baseUrl);
+        await runner.verifyUrlContains(envData.baseUrl);
+        await runner.verifyElementIsVisible(homePage.logo);
+        await runner.validateAttribute(homePage.logo, "src", homeData.logoSrc);
+        await runner.validateAttribute(homePage.logo, "alt", homeData.logoAlt);
       });
 
       test("Verify something", async ({ runner, homePage }) => {
-        await runner.verifyElementIsVisible(homePage.homePageLogo);
-        // Check the logo's src attribute using runner
-        await runner.validateAttribute(
-          homePage.homePageLogo,
-          "src",
-          "https://ecommerce-playground.lambdatest.io/image/catalog/maza/svg/image2vector.svg"
-        );
+        await runner.verifyElementIsVisible(homePage.logo);
       });
     });
   }
--- utilities/envData.ts
+++ utilities/envData.ts
@@ -1,4 +1,7 @@
 import { Page } from "@playwright/test";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export class EnvData {
   readonly baseUrl: string;
